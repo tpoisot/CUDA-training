84557839567c5efa2225a003ce283f6a1336fa69
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2012-04-09 20:22:53 -0400

    add + mussel

---
+++ simple/add2.cu
@@ -0,0 +1,64 @@
+//: nvcc add2.cu -o add2
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N       1000000
+#define BLOCKS  1000
+#define THREADS 1000  
+
+__global__ void add(int *a, int *b, int *c)
+{
+    int tid = threadIdx.x + blockDim.x * blockIdx.x; // bloakcDim.x := 1000
+    if (tid < N)
+    {
+        c[tid] = a[tid] + b[tid];
+    }
+}
+
+int main(int argc, char **argv)
+{
+    // Memory on the host:
+    const size_t nbytes = N * sizeof(int);
+    int *h_a = (int*)malloc(nbytes);
+    int *h_b = (int*)malloc(nbytes);
+    int *h_c = (int*)malloc(nbytes);
+
+    // Memory on the device:
+    int *d_a, *d_b, *d_c;
+    cudaMalloc((void**)&d_a, nbytes);
+    cudaMalloc((void**)&d_b, nbytes);
+    cudaMalloc((void**)&d_c, nbytes);
+
+    // Fill the arrays:
+    for (int i = 0; i < N; ++i)
+    {
+        h_a[i] = i;
+        h_b[i] = 2 * i * i - 6 * i;
+    }
+
+    // Copy from the host to the device:
+    cudaMemcpy(d_a, h_a, nbytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, nbytes, cudaMemcpyHostToDevice);
+
+    // Call CUDA add:
+    add<<<BLOCKS,THREADS>>>(d_a, d_b, d_c);
+
+    // Copy from the device to the host:
+    cudaMemcpy(h_c, d_c, nbytes, cudaMemcpyDeviceToHost);
+
+    // Print the results:
+    for (int i = 0; i < N; ++i)
+    {
+        printf("(+ %d %d) -> %d\n", h_a[i], h_b[i], h_c[i]);
+    }
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+    free(h_a);
+    free(h_b);
+    free(h_c);
+    return EXIT_SUCCESS;
+}
+
