cbcd4b199dda410d22d30e968e699c967641805e
Tim Poisot <timothee.poisot@uqar.ca>
2012-04-10 09:11:06 -0400

    test

--- simple/add0.cu
+++ simple/add0.cu
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
     int c;
     int *dev_c;
     cudaMalloc((void**)&dev_c, sizeof(int));
-    cuda_add<<<1,1>>>(2, 7, dev_c);
+    cuda_add<<<1,1>>>(2, 2, dev_c);
     cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
     printf("Almighty CUDA's answer: 2 + 2 = %d.\n", c);
     cudaFree(dev_c);
